c4ce7f95786be23afed244a7b1c0b9a78d95be9b
Oneleven <wuduyouwo@gmail.com>
2017-05-11 22:45:10 +0800

    添加了一个页面

---
+++ 一个页面/js/main.js
@@ -0,0 +1,20 @@
+// 垂直轮播
+$('#demo15').swiper({
+    pagination: '#demo15 .swiper-pagination',
+    direction: 'vertical',
+    slidesPerView: 1,
+    paginationClickable: true,
+    spaceBetween: 30,
+    mousewheelControl: true,
+    autoplay: 3500,
+    loop: true
+  });
+
+//水平轮播
+$('#demo5').swiper({
+    pagination: '#demo5 .swiper-pagination',
+    slidesPerView: 3,
+    paginationClickable: true,
+    spaceBetween: 30,
+    loop: true
+  });
