8ce9a597b3973101df9007a80d3dead23648cb72
Oneleven <wuduyouwo@gmail.com>
2017-06-16 13:49:47 +0800

    添加require.js小demo

--- demo-test/demo_require/main.js
+++ demo-test/demo_require/main.js
@@ -1,10 +1,12 @@
 require.config({
         paths: {
-            'jquery': './node_modules/dist/jquery'
+            'jquery': 'lib/jquery'
         }
-    }
-)
+    })
 
 require(['jquery'], function ($) {
-    $('h1').textContent = 'Hello World'
+    $(function(){
+         $('h1').text ('Hello World')
+    })
+   
 })
